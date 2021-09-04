40b259c21d1ddefc04cbe83486a20eba247e0340
Evan Cameron <cameron.evan@gmail.com>
2021-09-04 14:53:16 -0400

    begin doh

--- bin/src/sender.rs
+++ bin/src/sender.rs
@@ -5,6 +5,7 @@ use std::{
 };
 
 use anyhow::{Context, Result};
+use bytes::{Bytes, BytesMut};
 use governor::{
     clock::DefaultClock,
     state::{InMemoryState, NotKeyed},
@@ -14,7 +15,14 @@ use parking_lot::Mutex;
 use tokio::{
     io::AsyncWriteExt,
     net::{tcp::OwnedWriteHalf, UdpSocket},
+    sync::mpsc,
     task,
+};
+use trust_dns_proto::{
+    error::ProtoError,
+    https::HttpsClientStream,
+    op::Message,
+    xfer::{DnsRequest, DnsRequestSender, DnsResponse, FirstAnswer},
 };
 
 use crate::{
@@ -76,7 +84,6 @@ impl Sender {
 }
 
 // a very simple DNS message sender
-#[derive(Debug)]
 pub enum MsgSend {
     Tcp {
         s: OwnedWriteHalf,
@@ -84,6 +91,9 @@ pub enum MsgSend {
     Udp {
         s: Arc<UdpSocket>,
         target: SocketAddr,
+    },
+    Doh {
+        s: mpsc::Sender<Message>,
     },
 }
 
@@ -98,6 +108,54 @@ impl MsgSend {
             MsgSend::Udp { s, target } => {
                 s.send_to(msg, *target).await?;
             }
+            MsgSend::Doh { s } => {
+                s.send(todo!()).await?;
+            }
+        }
+        Ok(())
+    }
+}
+
+pub struct DohSender {
+    rx: mpsc::Receiver<Message>,
+    stream: HttpsClientStream,
+    resp_tx: mpsc::Sender<(Bytes, SocketAddr)>,
+}
+
+impl DohSender {
+    pub async fn new(
+        target: SocketAddr,
+    ) -> (
+        Self,
+        mpsc::Sender<Message>,
+        mpsc::Receiver<(Bytes, SocketAddr)>,
+    ) {
+        let (tx, rx) = mpsc::channel(100_000);
+        let (resp_tx, resp_rx) = mpsc::channel(100_000);
+        (
+            Self {
+                rx,
+                resp_tx,
+                stream: todo!(),
+            },
+            tx,
+            resp_rx,
+        )
+    }
+    pub async fn run(&mut self) -> Result<()> {
+        while let Some(msg) = self.rx.recv().await {
+            let req = DnsRequest::new(todo!(), Default::default());
+            let stream = self.stream.send_message(req);
+            let tx = self.resp_tx.clone();
+            tokio::spawn(async move {
+                let resp = stream.first_answer().await;
+                // TODO: unfortunately (for us) trustdns is deserializing the message
+                // so we have to convert it back to a vec in order to use it..
+                if let Ok(resp) = resp {
+                    // tx.send(resp).await?;
+                    todo!();
+                }
+            });
         }
         Ok(())
     }
